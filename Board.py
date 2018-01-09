2aa5f63fdb3bddb805a577db58ae37e51b31ff73
jhperson1 <jessica_huang@college.harvard.edu>
2018-01-08 21:23:57 -0500

    new board class

---
+++ Board.py
@@ -0,0 +1,18 @@
+#!/user/bin/env/Python
+
+class Board():
+    def __init__(self, values):
+        # the underscore indicates that the user shouldn't
+        # directly call the board attribute. instead the
+        # user should use the methods of the board class
+        _board = []
+        if ():
+        else:
+
+            type, nameerror
+
+    def getValue(self):
+
+    def addValue(self):
+
+    def print(self):
