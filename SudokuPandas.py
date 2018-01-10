b09e7720611b5c2308333ece4bca1da3ecdee55b
jhperson1 <jessica_huang@college.harvard.edu>
2018-01-10 16:34:20 -0500

    implemented logic for pandas, did not use syntax

--- SudokuPandas.py
+++ SudokuPandas.py
@@ -2,6 +2,8 @@
 
 import pandas
 
+# choices is the dataframe
+
 def _addChoices():
     choices = pandas.dataframe({"val", [1,9]}, {"row", [1,9]}, {"col", [1,9]}, {"choice", [0,1]})
 
@@ -12,7 +14,9 @@ def _addConstraints():
             t = choices.where("row" == i, "col" == j)
             prob += LpSum([t]) == 1, ""
 
-    # row and column constraint: forall i,k: sum_j {c_ijk} == 1
+    # row and column constraint:
+    # forall i,k: sum_j {c_ijk} == 1
+    # forall k,k: sum_i {c_ijk} == 1
     for i in range(1,10):
         for k in range(1,10):
             t = choices.where("row" == i, "val" == k)
@@ -24,5 +28,11 @@ def _addConstraints():
             prob += LpSum([t]) == 1, ""
 
     # box constraint: forall k, boxes: sum_(i,j) {c_ijk} == 1
+    for box in boxes:
+        for k in range(1,10):
+            prob += LpSum([choices.where("row" == r, "col" == c, "val" == k)] for (r,c) in box)
 
-def _addBoard():
+def _addBoard(self, board):
+    for i in range(9):
+        for j in range(9):
+            self.choices = board[i][j]
